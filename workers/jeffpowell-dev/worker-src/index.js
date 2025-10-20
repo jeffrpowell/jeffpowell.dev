09048d64a1a4d2ecfab6012bcb0843cd83a03f24
Jeff Powell <github@jeffpowell.dev>
2025-10-20 14:31:54 -0600

    removing hard-coded allow list of static assets, given R2 class B free tier is more permissive than the worker invocation limit

--- workers/jeffpowell-dev/worker-src/index.js
+++ workers/jeffpowell-dev/worker-src/index.js
@@ -1,19 +1,3 @@
-const ALLOW_LIST = [
-    'aoc.jpg',
-    'backup.jpg',
-    'commandPrompt.png',
-    'pdf.png',
-    'repo_website.jpg',
-    'JeffPowell_CliftonStrengths.pdf',
-    'JeffPowell_DiSC.pdf',
-    'JeffPowell_RHETI.pdf',
-    'JeffPowell_SocialStyles.pdf',
-    'JeffPowell_StrengthsProfile.pdf',
-    'JeffPowell_CliftonStrengths.pdf',
-    'JeffPowell_MTDISC.pdf',
-    'ListawayWordmarkLight.png'
-];
-
 async function handleRequest(request, env, ctx) {
     const url = new URL(request.url);
     const path = url.pathname.slice(1);
@@ -26,10 +10,6 @@ async function handleRequest(request, env, ctx) {
     
     // Extract filename from /assets/[filename]
     const filename = path.slice('assets/'.length);
-    
-    if (!ALLOW_LIST.includes(filename)) {
-        return new Response("Not Found", { status: 404 });
-    }
 
     const cacheKey = `https://${url.hostname}${url.pathname}`;
     const cache = caches.default;
